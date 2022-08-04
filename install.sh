e7914762b0f5e8150c1867cda01d5532856511ea
manuel <manuel@manux.loc>
2022-08-04 23:47:25 +0200

    install dotfiles

--- install.sh
+++ install.sh
@@ -1,11 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -xeu
 
 sudo apt-get install \
     zsh              \
-    tmux
+    tmux             \
+    meld
 
 # install oh-my-zsh (only if not installed yet)
 if [ -z "$ZSH" ]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+# setup dotfiles
+ln -s $SCRIPT_DIR/zshrc ~/.zshrc
+
+
 
